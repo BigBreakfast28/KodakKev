5a80bd70d97bc990cef077ef3fe26d2f251e30f0
Cameron <deancameron99@gmail.com>
2024-06-18 13:12:37 -0400

    Adding a couple of elements to the portfolio page

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 
 
-function openForm() {
-    document.getElementById("contactInfo").style.display = "block";
-}
 
-function closeForm() {
-    document.getElementById("contactInfo").style.display = "none";
-}
 
 
 
