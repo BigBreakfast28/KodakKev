89969b995e24136415932f5af8ed073d8a845dfe
Cameron <deancameron99@gmail.com>
2024-06-23 17:46:55 -0400

    Adding variables to gallery images

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const boudoirPic = document.getElementsByClassName('boudoir');
 const brandingPic = document.getElementsByClassName('branding');
 const documentaryPic = document.getElementsByClassName('documentary');
+const eventPic = document.getElementsByClassName('event');
+const familyPic = document.getElementsByClassName('family');
+const headShotPic = document.getElementsByClassName('headshot');
+const lifeStylePic = document.getElementsByClassName('lifestyle');
+const lovePic = document.getElementsByClassName('love');
+const petPic = document.getElementsByClassName('pet');
+const portraitPic = document.getElementsByClassName('portrait');
+const sportsPic = document.getElementsByClassName('sports');
+const studioPic = document.getElementsByClassName('studio');
+const waterShadowPic = document.getElementsByClassName('watershadow');
+const wildLifePic = document.getElementsByClassName('wildlife');
 
 document.getElementById('pagePort2').onclick = function() {
     let galleryPage2 = document.getElementById('gallery');
