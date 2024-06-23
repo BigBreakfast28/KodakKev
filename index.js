c1d1116326c97ca6fadae9e1cd36d5d6de964ce4
Cameron <deancameron99@gmail.com>
2024-06-23 17:33:24 -0400

    Completed adding all gallery images to portfolio page

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const boudoirPic = document.getElementsByClassName('boudoir');
+const brandingPic = document.getElementsByClassName('branding');
+const documentaryPic = document.getElementsByClassName('documentary');
 
+document.getElementById('pagePort2').onclick = function() {
+    let galleryPage2 = document.getElementById('gallery');
+    galleryPage2.innerHTML = "Testing";
+   
+}
 
 
 
